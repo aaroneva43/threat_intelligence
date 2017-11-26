3c7bdec7e1edd91e556acd7b08c8d6d0ec0cb18e
Aaron <xiepeng.cn@gmail.com>
2017-11-26 21:34:04 +0800

    minor change

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,5 +2,5 @@ import React, { Component, createElement } from 'react'
 import css from './Main.scss'
 
 export default () => {
-    return <div className={css.foo}>Main</div>
+    return <div className={css.foo}>Contents to be rendered here</div>
 }
