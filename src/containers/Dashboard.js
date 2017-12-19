a9a473a7842a8eb7719538cd10113cfc016a9290
Deefay Young <deefay@DeefaymatoMacBook-Pro.local>
2017-12-19 20:56:41 +0800

    nav style

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,8 +1,47 @@
-import React, { Component, createElement } from 'react'
+import React, { Component, createElement } from 'react';
 import { connect } from 'react-redux';
+import { Row, Col } from 'antd';
 
 const Dashboard = () => {
-    return <div style={{ height: 10000, width: 1075, background:'#fff' }}>Dashboard</div>
+    return <div>
+ 
+            <Row type="flex" justify="start">
+                <Col span={12}>
+                    <div style={{background:'#fff' }}>
+                        Host
+                    </div>      
+                </Col>
+                <Col span={12}>
+                    <div style={{background:'#aaa' }}>
+                        Host
+                    </div>  
+                </Col>
+            </Row>
+            <Row>
+                <Col span={8}>
+                    <div style={{background:'#3eda89' }}>
+                         three      
+                    </div>            
+                </Col>
+                <Col span={8}>
+                    <div style={{background:'#545454' }}>
+                         three      
+                    </div>            
+                </Col>
+                <Col span={8}>
+                    <div style={{background:'#919191' }}>
+                         three      
+                    </div>            
+                </Col>
+            </Row>
+            <Row>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+            </Row>
+
+    </div>
 }
 
 export default Dashboard
--- src/containers/Host.js
+++ src/containers/Host.js
@@ -2,7 +2,16 @@ import React, { Component, createElement } from 'react'
 import { connect } from 'react-redux';
 
 const Host = () => {
-    return <div style={{ height: 10000, width: 1075, background:'#fff' }}>Host</div>
+    return <div style={{ height: 10000, background:'#fff' }}>
+    Host
+    
+    
+    
+    
+    
+    
+    
+    </div>
 }
 
 export default Host
