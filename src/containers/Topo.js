ef2e007fd2a3167ce48b3ccf7277ccfc2465dd1f
Aaron <xiepeng.cn@gmail.com>
2018-01-01 13:45:50 +0800

    fix topo

--- src/containers/Topo.js
+++ src/containers/Topo.js
@@ -5,48 +5,50 @@ import { Table, Icon } from 'antd'
 
 class Topo extends PureComponent {
 
-    componentDidMount() {
+  componentDidMount() {
 
 
-        // create an array with nodes
-        var nodes = new vis.DataSet([
-          {id: 1, label: 'Node 1'},
-          {id: 2, label: 'Node 2'},
-          {id: 3, label: 'Node 3'},
-          {id: 4, label: 'Node 4'},
-          {id: 5, label: 'Node 5'}
-        ]);
+    // create an array with nodes
+    var nodes = new vis.DataSet([
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+      { id: 5, label: 'Node 5' }
+    ]);
 
-        // create an array with edges
-        var edges = new vis.DataSet([
-          {from: 1, to: 3},
-          {from: 1, to: 2},
-          {from: 2, to: 4},
-          {from: 2, to: 5},
-          {from: 3, to: 3}
-        ]);
+    // create an array with edges
+    var edges = new vis.DataSet([
+      { from: 1, to: 3 },
+      { from: 1, to: 2 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 },
+      { from: 3, to: 3 }
+    ]);
 
-        // create a network
-        var graphArea = document.getElementById('topo');
-        var data1 = {
-          nodes: nodes,
-          edges: edges
-        };
-        var options = {};
-        var network = new vis.Network(graphArea, data1, options);
-    }
+    // create a network
+    var graphArea = document.getElementById('topo');
+    var data1 = {
+      nodes: nodes,
+      edges: edges
+    };
+    var options = {};
+    var network = new vis.Network(graphArea, data1, options);
+  }
 
-    render {
 
-      return (
-        <div id="topo" style={ {height:'100%', background:'#fff'}}></div>
+  render() {
 
-        
+    return (
+      <div id="topo" style={{ height: '100%', background: '#fff' }}></div>
+
+
     )
 
 
-
-    }
-
-    }
+  }
 }
+
+
+export default Topo
+
