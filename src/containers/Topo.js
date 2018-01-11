060936d360df0df4318531025325de88ed566e0f
Deefay Young <aaroneva43@163.com>
2018-01-10 22:05:35 -0800

    topo modify

--- src/containers/Topo.js
+++ src/containers/Topo.js
@@ -10,43 +10,128 @@ class Topo extends PureComponent {
     componentDidMount() {
 
         const { getConfig, exitConfig } = this.props
-
+        
         const me = this
 
+        var graphData = [
+            {
+              "deviceType": "Host", 
+              "ip": "172.16.0.2", 
+              "key": "172.16.0.2", 
+              "numOfApps": 3, 
+              "numOfOpenPorts": 4, 
+              "osType": "Ubuntu 16.04", 
+              "route": "192.168.3.2,192.168.3.1,172.16.0.2", 
+              "vulns": {
+                "critical": 0, 
+                "high": 0, 
+                "low": 0, 
+                "medium": 0
+              }
+            }, 
+            {
+              "deviceType": "Host", 
+              "ip": "192.168.1.69", 
+              "key": "192.168.1.69", 
+              "numOfApps": 6, 
+              "numOfOpenPorts": 8, 
+              "osType": "Windows 10 Home 16299", 
+              "route": "192.168.1.78,192.168.1.69", 
+              "vulns": {
+                "critical": 0, 
+                "high": 0, 
+                "low": 16, 
+                "medium": 0
+              }
+            }, 
+            {
+              "deviceType": "Host", 
+              "ip": "130.207.160.173", 
+              "key": "130.207.160.173", 
+              "numOfApps": "", 
+              "numOfOpenPorts": "", 
+              "osType": "", 
+              "route": "", 
+              "vulns": {
+                "critical": 0, 
+                "high": 0, 
+                "low": 1, 
+                "medium": 0
+              }
+            }, 
+            {
+              "deviceType": "Host", 
+              "ip": "164.67.228.152", 
+              "key": "164.67.228.152", 
+              "numOfApps": "", 
+              "numOfOpenPorts": 2, 
+              "osType": "CentOS 6", 
+              "route": "192.168.1.78,75.49.252.1,71.148.134.57,71.145.0.206,12.83.39.145,12.122.114.5,* * *,4.15.122.46,137.164.11.31,137.164.11.0,137.164.11.36,137.164.11.23,* * *,169.232.4.8,169.232.4.53,* * *,164.67.134.252,164.67.228.152", 
+              "vulns": {
+                "critical": 0, 
+                "high": 0, 
+                "low": 16, 
+                "medium": 0
+              }
+            }, 
+            {
+              "deviceType": "Host", 
+              "ip": "54.192.117.243", 
+              "key": "54.192.117.243", 
+              "numOfApps": "", 
+              "numOfOpenPorts": 2, 
+              "osType": "", 
+              "route": "192.168.1.78,75.49.252.1,71.148.134.57,71.145.0.206,12.83.39.145,12.122.137.213,206.121.188.34,* * *,72.21.222.2,* * *,54.192.117.243", 
+              "vulns": {
+                "critical": 0, 
+                "high": 0, 
+                "low": 10, 
+                "medium": 0
+              }
+            }
+          ]
+
+
+
+
+
+
+
+// -------------- graph code begin ---------------------------------------------
+      
         var nodes = null;
         var edges = null;
         var network = null;
-
-        var LENGTH_MAIN = 350,
-            LENGTH_SERVER = 150,
-            LENGTH_SUB = 50,
-            WIDTH_SCALE = 2,
-            GREEN = 'green',
-            RED = '#C5000B',
-            ORANGE = 'orange',
-            //GRAY = '#666666',
-            GRAY = 'gray',
-            BLACK = '#2B1B17';
 
         // Called when the Visualization API is loaded.
 
         // Create a data table with nodes.
         nodes = [];
+        
+        for( var i = 0; i < graphData.length; i++){
+            var nodeData = graphData[i]
+            
+            var color = null
 
-        function addNodes(nodes){
-          var nodeLabel =  '130.207.160.173'
+            if( (nodeData.vulns.critical == 0) && (nodeData.vulns.high == 0) 
+                && (nodeData.vulns.medium == 0) && (nodeData.vulns.low == 0) ){
+                color = '#689962'
+            } else if ( (nodeData.vulns.critical == 0) && (nodeData.vulns.high == 0) 
+                && (nodeData.vulns.medium == 0) && (nodeData.vulns.low > 0)
+            ){
+                color = '#5088d5'
+            }
 
-          nodes.push({
-            id: 1,
-            label: nodeLabel,
-            group: 'switch'
-          });
-
-          return nodes;
+            nodes.push({
+                id: graphData[i].ip, 
+                label: graphData[i].ip,
+                shape: "dot",
+                color: color,
+                fixed: false, 
+                physics:false
+            });
         
         }
-
-        addNodes(nodes)
         // Create a data table with links.
         edges = [];
 
@@ -64,7 +149,7 @@ class Topo extends PureComponent {
                 }
             },
             edges: {
-                color: GRAY,
+                color: "#24e219",
                 smooth: false
             },
             physics: {
@@ -82,7 +167,10 @@ class Topo extends PureComponent {
                 },
                 'desktop': {
                     shape: 'dot',
-                    color: "#2B7CE9" // blue
+                    //color: "#2B7CE9" // blue
+                },
+                'red': {
+                    color: "#24e219" // blue
                 },
                 'mobile': {
                     shape: 'dot',
@@ -114,16 +202,18 @@ class Topo extends PureComponent {
         network.on("doubleClick", function (params) {
             params.event = "[original event]";
 
-            const entry = 'app_info/' + '172.16.0.2'
+            var nodeString = JSON.stringify(params, null, 4)
+            
+                        console.log('ip: ' + JSON.parse(nodeString).nodes);           
+
+            const entry = 'app_info/' + JSON.parse(nodeString).nodes
             getConfig(entry)
             me.setState({ entry })
-            //document.getElementById('eventSpan');
-            //.innerHTML = '<h2>Click event:</h2>' + JSON.stringify(params, null, 4);
-            // console.log('double click event, getNodeAt returns: ' + JSON.stringify(params, null, 4));
+
+
         });
-
+// --------------------  graph code end ------------------------------------------------
     }
-
 
     render() {
 
@@ -136,6 +226,8 @@ class Topo extends PureComponent {
                 <Modal
                     visible={!!config[entry]}
                     onCancel={() => { exitConfig(entry) }}
+                    footer={null}
+                    title="设备信息"
                 >
                     {JSON.stringify(config[entry])}
                 </Modal>
