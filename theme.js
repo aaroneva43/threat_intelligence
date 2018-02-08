f3a888a946c1356e3f6445eeb539d11a32146f8a
pengxie <pengxie@fortinet.com>
2018-02-08 19:56:52 +0800

    localize antd icons

--- theme.js
+++ theme.js
@@ -3,5 +3,6 @@ module.exports = () => {
         'primary-color': 'darkgreen',
         'link-color': 'red',
         'border-radius-base': '2px',
+        'icon-url': '"/public/fonts/iconfont/iconfont"',
     }
 }
